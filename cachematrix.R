439550579232f211094208fd64e9c3f4992d070c
Irina Gorodetskaya <irinagorodetskaya@Irinas-MacBook-Air.local>
2015-04-22 12:00:06 +0400

    Programming assignment 2 completed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function enriches a matrix x with 4 functions that can cache the matrix and its inverse
+## in a different environment and return them to the console.
+## The second function checks if the inverse of a matrix x has been cached and retuns it if so,
+## calculates and returns the inverse otherwise.
 
-## Write a short comment describing this function
+
+
+## The function makeCacheMatrix below returns 4 other functions:
+## get() returns x - the the formal argument of the main function makeCacheMatrix,
+## set(y) assigns y to x in a different environment and assigns NULL to the local variable inv,
+## setinv(inverse) assigns inverse to inv in a different environment
+## getinv() returns inv from the other environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+            x <<- y
+            inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+## The function cacheSolve below uses the functions from makeCacheMatrix to
+## assign the cached inverse matrix to a local variable inv.
+## Then it checks that there was something cached and returns it if so,
+## if not calculates the inverse, chaches it, and returns to the console.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	inv <- x$getinv()
+    if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    ## Return a matrix that is the inverse of 'x
+    inv
 }
